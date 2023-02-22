fab0c558c19709cb414d33c9430b04b3b62c4cf1
nilsmango <0@project7iii.ch>
2023-02-22 14:12:50 +0100

    Initial Commit

---
+++ MementoMori/MementoMoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  MementoMoriApp.swift
+//  MementoMori
+//
+//  Created by Simon Lang on 22.02.23.
+//
+
+import SwiftUI
+
+@main
+struct MementoMoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
