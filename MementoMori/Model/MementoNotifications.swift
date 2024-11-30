78b8b32397381a22839798ae76b736c03cd53a80
nilsmango <simonmilagrolang@icloud.com>
2024-11-30 19:18:30 +0100

    Added more quotes, made the first 26 not random.

--- MementoMori/Model/MementoNotifications.swift
+++ MementoMori/Model/MementoNotifications.swift
@@ -10,7 +10,20 @@ import UserNotifications
 
 class MementoNotifications {
     
-    let randomQuotes = ["It's later than you think", "Waste no more time arguing what a good man should be. Be One. – Marcus Aurelius", "It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own. – Marcus Aurelius", "If a man knows not which port he sails, no wind is favorable. - Seneca", "We are more often frightened than hurt; and we suffer more in imagination than in reality. – Seneca", "I begin to speak only when I’m certain what I’ll say isn’t better left unsaid. – Cato", "How long are you going to wait before you demand the best for yourself? – Epictetus", "That man lives badly who does not know how to die well. - Seneca", "I cannot escape death, but at least I can escape the fear of it. — Epictetus", "It is not death that a man should fear, but rather he should fear never beginning to live. — Marcus Aurelius", "I am not afraid of death, I just don’t want to be there when it happens. – Woody Allen", "Do not fear death so much but rather the inadequate life. – Bertolt Brecht", "To be evenminded is the greatest virtue. - Heraclitus", "Just keep in mind: the more we value things outside our control, the less control we have. - Epictetus", "The tranquility that comes when you stop caring what they say. Or think, or do. Only what you do. - Marcus Aurelius", "The whole future lies in uncertainty: live immediately. - Seneca", "It does not matter what you bear, but how you bear it. - Seneca", "To be calm is the highest achievement of the self. - Zen proverb", "He has the most who is content with the least. - Diogenes", "Self-control is strength. Right thought is mastery. Calmness is power. - James Allen", "Man conquers the world by conquering himself. - Zeno of Citium", "Be stoic: Just do the right thing. Just keep going. - Maxime Lagacé", "Success is based off of your willingness to work your ass off no matter what obstacles are in your way. - David Goggins", "When someone is properly grounded in life, they shouldn’t have to look outside themselves for approval. - Epictetus", "You have power over your mind — not outside events. Realize this, and you will find strength. - Marcus Aurelius", "Remembering that you are going to die is the best way I know to avoid the trap of thinking you have something to lose. - Steve Jobs"]
+    func loadQuotes() -> [String] {
+        guard let url = Bundle.main.url(forResource: "stoics", withExtension: "json") else {
+            return []
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decodedData = try JSONDecoder().decode(QuotesData.self, from: data)
+            return decodedData.quotes
+        } catch {
+            print("Error decoding JSON: \(error)")
+            return []
+        }
+    }
     
     let center = UNUserNotificationCenter.current()
     
@@ -51,7 +64,6 @@ class MementoNotifications {
             // Enable or disable features based on the authorization.
         }
         
-        
         center.removeAllPendingNotificationRequests()
         
         if active {
@@ -86,9 +98,20 @@ class MementoNotifications {
                     randomTimes += secondTimes
             }
             
-            for timeDict in randomTimes {
-                let randomIndex = Int.random(in: 0..<randomQuotes.count)
-                let quote = randomQuotes[randomIndex]
+            let quotes = loadQuotes()
+            let numberToRemainInOrder = 26
+            let first26Quotes = Array(quotes.prefix(numberToRemainInOrder))
+            let restQuotes = Array(quotes.dropFirst(numberToRemainInOrder))
+            let randomQuotes = first26Quotes + restQuotes.shuffled()
+            let randomQuoteCount = randomQuotes.count
+            
+            for (index, timeDict) in randomTimes.enumerated() {
+                let quote: String
+                if index < randomQuoteCount {
+                    quote = randomQuotes[index]
+                } else {
+                    quote = randomQuotes.randomElement()!
+                }
                 
                 let content = UNMutableNotificationContent()
                 content.title = mementoText
@@ -107,9 +130,6 @@ class MementoNotifications {
                 let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
                 
                 center.add(request)
-                
-                print(dateComponents)
-                
             }
         }
     }
---
+++ MementoMori/Model/QuotesData.swift
@@ -0,0 +1,12 @@
+//
+//  QuotesData.swift
+//  MementoMori
+//
+//  Created by Simon Lang on 30.11.2024.
+//
+
+import Foundation
+
+struct QuotesData: Codable {
+    let quotes: [String]
+}
